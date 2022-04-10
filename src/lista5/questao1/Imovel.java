f2a5241e294c33243e1654874b26fa18bdf97dae
Guilherme <57736435+felixdasilva050@users.noreply.github.com>
2022-04-10 03:13:26 -0300

    questão 1 resolvida

---
+++ src/lista5/questao1/Imovel.java
@@ -0,0 +1,22 @@
+package lista5.questao1;
+
+public class Imovel {
+    private String endereco;
+    private Double preco;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
